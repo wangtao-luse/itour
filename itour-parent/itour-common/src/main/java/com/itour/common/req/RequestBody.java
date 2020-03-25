befb3b04bdf10110aad380e8690ff089a7a5fa4d
wwang-luse <wwangtaoc11@163.com>
2020-03-25 14:36:41 +0800

    init

---
+++ itour-parent/itour-common/src/main/java/com/itour/common/req/RequestBody.java
@@ -0,0 +1,69 @@
+package com.itour.common.req;
+
+import java.io.Serializable;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class RequestBody implements Serializable {
+private static final long serialVersionUID = 1L;
+private JSONObject content;
+/**
+ * 用户唯一号
+ */
+private Integer uId;
+/**
+ * 第三方登录唯一ID
+ */
+private String oauthId;
+/**
+ * 第三方登录唯一标识(手机：phone;邮箱：email;QQ:qq;微信：wechat;用户名：uid;微博:weibo)
+ */
+private String oauthType;
+
+/**
+ * 昵称
+ */
+private String nickname;
+/**
+* 头像
+*/
+private String avatar;
+public JSONObject getContent() {
+	return content;
+}
+public void setContent(JSONObject content) {
+	this.content = content;
+}
+public Integer getuId() {
+	return uId;
+}
+public void setuId(Integer uId) {
+	this.uId = uId;
+}
+public String getOauthId() {
+	return oauthId;
+}
+public void setOauthId(String oauthId) {
+	this.oauthId = oauthId;
+}
+public String getOauthType() {
+	return oauthType;
+}
+public void setOauthType(String oauthType) {
+	this.oauthType = oauthType;
+}
+public String getNickname() {
+	return nickname;
+}
+public void setNickname(String nickname) {
+	this.nickname = nickname;
+}
+public String getAvatar() {
+	return avatar;
+}
+public void setAvatar(String avatar) {
+	this.avatar = avatar;
+}
+
+
+}
---
+++ itour-parent/itour-common/src/main/java/com/itour/common/resp/ResponseMessage.java
@@ -0,0 +1,98 @@
+package com.itour.common.resp;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.itour.common.constant.ConstantBase;
+
+
+
+public final class ResponseMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 00:失败
+	 * 10:成功
+	 */
+	private String resultCode;
+	private String resultMessage;
+	private Map<String, Object> returnResult = new HashMap<String, Object>();
+	public ResponseMessage() {
+	}
+	public ResponseMessage(String resultCode, String resultMessage) {
+		super();
+		this.resultCode = resultCode;
+		this.resultMessage = resultMessage;
+	}
+	public ResponseMessage(Map map) {
+		super();
+		this.resultCode = ConstantBase.SUCCESS_CODE;
+		this.resultMessage = ConstantBase.SUCESS_MESSAGE;
+		this.returnResult=map;
+	}
+	public ResponseMessage(Object object) {
+		super();
+		this.resultCode = ConstantBase.SUCCESS_CODE;
+		this.resultMessage = ConstantBase.SUCESS_MESSAGE;
+		this.returnResult.put("result", object);
+	}
+	public String getResultCode() {
+		return resultCode;
+	}
+	public void setResultCode(String resultCode) {
+		this.resultCode = resultCode;
+	}
+	public String getResultMessage() {
+		return resultMessage;
+	}
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+	public Map<String, Object> getReturnResult() {
+		return returnResult;
+	}
+	public void setReturnResult(Map<String, Object> returnResult) {
+		this.returnResult = returnResult;
+	}
+
+	 //添加可变参数注意是为了解决Could not extract response: no suitable HttpMessageConverter found for respose type
+	//具体原因不清楚。。。。。待解决
+	  public void setReturnResult(Object object,Object...obj) { 
+		  Map<String,Object> map = new  HashMap<String, Object>();
+		  map.put("result", object);
+		  this.returnResult = map;
+	  }
+	  
+	 
+	public void setReturnResult(String key, Object object) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(key, object);
+		this.returnResult = map;
+	}
+
+	public  ResponseMessage add(Object object) {
+		this.returnResult.put("data", object);
+		return this;
+	}
+
+	public ResponseMessage add(String key, Object object) {
+	     returnResult.put(key, object);
+		return this;
+	}
+
+	public static ResponseMessage getSucess() {
+		return new ResponseMessage(ConstantBase.SUCCESS_CODE, ConstantBase.SUCESS_MESSAGE);
+	}
+	public static ResponseMessage getSucess(String successMsg) {
+		return new ResponseMessage(ConstantBase.SUCCESS_CODE, successMsg);
+	}
+
+	public static ResponseMessage getFailed() {
+		return new ResponseMessage(ConstantBase.FAILED_CODE, ConstantBase.FAILED_MESSAGE);
+	}
+	public static ResponseMessage getFailed(String faileMsg) {
+		return new ResponseMessage(ConstantBase.FAILED_CODE, faileMsg);
+	}
+
+}
