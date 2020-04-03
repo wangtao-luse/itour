bd8d816b071ee0037c24eac4df44e3af95f539f0
wwang-luse <wwangtaoc11@163.com>
2020-04-03 08:48:00 +0800

    itour

--- itour-parent/itour-online-web/src/main/java/com/itour/OnlienApplication.java
+++ itour-parent/itour-online-web/src/main/java/com/itour/OnlienApplication.java
@@ -3,10 +3,8 @@ package com.itour;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableFeignClients
 public class OnlienApplication {
 
 
