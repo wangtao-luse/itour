43be71788ae87b2ece246544cdc16a35f9bd71ae
wangtao-luse <wwangtaoc11@163.com>
2020-04-10 07:24:32 +0800

    解决公共对象无法注入问题

--- itour-parent/itour-account-api/src/main/java/com/itour/account/api/AccountApi.java
+++ itour-parent/itour-account-api/src/main/java/com/itour/account/api/AccountApi.java
@@ -1,4 +1,4 @@
-package com.tour.account.api;
+package com.itour.account.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
