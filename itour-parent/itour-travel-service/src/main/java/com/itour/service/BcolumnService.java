d7eaad49805edc717e568e05a81d00912ca50aa7
wangtao-luse <wwangtaoc11@163.com>
2020-05-07 20:21:53 +0800

    init

--- itour-parent/itour-travel-service/src/main/java/com/itour/service/BcolumnService.java
+++ itour-parent/itour-travel-service/src/main/java/com/itour/service/BcolumnService.java
@@ -1,7 +1,14 @@
 package com.itour.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itour.common.req.RequestMessage;
 import com.itour.common.resp.ResponseMessage;
@@ -20,6 +27,8 @@ import com.itour.persist.BcolumnMapper;
  */
 @Service
 public class BcolumnService extends ServiceImpl<BcolumnMapper, Bcolumn> {
+	@Autowired
+	BcolumnMapper bcolumnMapper;
 	/**
 	 * 旅行博客专栏查询
 	 * @param requestMessage
@@ -28,6 +37,17 @@ public class BcolumnService extends ServiceImpl<BcolumnMapper, Bcolumn> {
 public ResponseMessage queryColumnList(RequestMessage requestMessage) {
 	ResponseMessage responseMessage = ResponseMessage.getSucess();
 	try {
+		 JSONObject jsonObject = requestMessage.getBody().getContent();
+		 JSONObject pageJson = jsonObject.getJSONObject("page");
+		 QueryWrapper<Bcolumn> queryWrapper = new QueryWrapper<Bcolumn>();
+		 if(null!=pageJson) {
+			 Page page = pageJson.toJavaObject(Page.class);
+			 Page selectPage = this.baseMapper.selectPage(page, queryWrapper); 
+			 responseMessage.setReturnResult(selectPage);
+		 }else {
+			 List<Bcolumn> selectList = this.baseMapper.selectList(queryWrapper);
+			 responseMessage.setReturnResult(selectList);
+		 }
 		
 	}catch (Exception e) {
 		// TODO: handle exception
@@ -41,10 +61,13 @@ public ResponseMessage queryColumnList(RequestMessage requestMessage) {
  * @param requestMessage
  * @return
  */
+@Transactional
 public ResponseMessage insertColumnList(RequestMessage requestMessage) {
 	ResponseMessage responseMessage = ResponseMessage.getSucess();
 	try {
-		
+		 JSONObject jsonObject = requestMessage.getBody().getContent();
+		 Bcolumn bcolumn = jsonObject.getJSONObject("vo").toJavaObject(Bcolumn.class);
+		 this.baseMapper.insert(bcolumn);
 	}catch (Exception e) {
 		// TODO: handle exception
 		e.printStackTrace();
@@ -60,7 +83,9 @@ public ResponseMessage insertColumnList(RequestMessage requestMessage) {
 public ResponseMessage updateColumnList(RequestMessage requestMessage) {
 	ResponseMessage responseMessage = ResponseMessage.getSucess();
 	try {
-		
+		 JSONObject jsonObject = requestMessage.getBody().getContent();
+		 Bcolumn bcolumn = jsonObject.getJSONObject("vo").toJavaObject(Bcolumn.class);
+		 this.baseMapper.updateById(bcolumn);
 	}catch (Exception e) {
 		// TODO: handle exception
 		e.printStackTrace();
@@ -73,10 +98,13 @@ public ResponseMessage updateColumnList(RequestMessage requestMessage) {
  * @param requestMessage
  * @return
  */
+@Transactional
 public ResponseMessage deleteColumnList(RequestMessage requestMessage) {
 	ResponseMessage responseMessage = ResponseMessage.getSucess();
 	try {
-		
+		 JSONObject jsonObject = requestMessage.getBody().getContent();
+		 Long id = jsonObject.getLong("id");
+		 this.baseMapper.deleteById(id);
 	}catch (Exception e) {
 		// TODO: handle exception
 		e.printStackTrace();
