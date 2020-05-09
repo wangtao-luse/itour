2f2636a0ac80de48b3c92cfafcbb68cf8baba859
wwang-luse <wwangtaoc11@163.com>
2020-05-09 13:34:37 +0800

    init

--- itour-parent/itour-online-web/src/main/resources/static/js/account/reg.js
+++ itour-parent/itour-online-web/src/main/resources/static/js/account/reg.js
@@ -1,4 +1,29 @@
 $(function(){
+
+/**
+ * 校验email
+ *  显示提示信息
+ */
+$("#form-email").focus(function(){
+	showDefault($(this));
+});
+/**
+ * 校验email
+ * 校验通过显示成功图标
+ * 校验失败提示错误信息
+ */
+$("#form-email").blur(function(){
+	var email=$(this).val();
+	if(test_email(email)){//校验通过		
+		showsucess($(this));
+	}else{//校验失败
+		//显示错误信息		
+		email?showError($(this),"form-email-error","格式错误"):"";
+	}
+	clearTip($(this));
+});
+
+/**注册提交*/
 $("#form-register").click(function(){
 	var url="/account/regSub";
 	var data=$.serializeObject($('#register-form'));	
@@ -10,3 +35,48 @@ $("#form-register").click(function(){
 });	
 	
 });
+/**
+ * 用于文本框错误信息提示
+ * @param $this 元素节点
+ * @param id    错误信息父元素的id
+ * @param errormsg 错误信息
+ * @returns
+ */
+function showError($this,id,errormsg){
+$this.parent().next().find("span").addClass("error").attr("id",id).html("<i class='i-error'></i>"+errormsg);
+}
+/**
+ * 校验成功后显示成功图标
+ * @param $this
+ * @returns
+ */
+function showsucess($this){
+	//1.添加成功的图标
+	$this.parent().addClass("form-item-valid");
+	//2.移除错误信息提示
+	$this.parent().next().find("span").html("").removeClass("error");
+}
+/**
+ * 显示默认的提示信息
+ * @param $this
+ * @returns
+ */
+function showDefault($this){
+	var df=$this.attr("default");
+	var hasError = $this.parent().next().find("span").hasClass("error");
+	if(!hasError){
+		$this.parent().next().find("span").html(df);
+	}
+}
+/**
+ * 清楚默认的提示信息
+ */
+function clearTip($this){
+	//清除默认的提示信息
+	var hasdef = $this.parent().next().find("span i").hasClass("i-def");
+	if(hasdef){
+		$this.parent().next().find("span").html("");
+	}
+	
+
+}
--- itour-parent/itour-online-web/src/main/resources/static/js/public.js
+++ itour-parent/itour-online-web/src/main/resources/static/js/public.js
@@ -88,3 +88,32 @@ var postAjax = function(url,postData,successFunction,options){
 		    });
 		    return o
 		};
+		
+/**
+ * 正则表达式
+ * 所有的通用的正则表达
+ */		
+var test_email = function(email){
+	/**
+	 * 邮箱格式:用户名称@域名
+	 * 用户名由数字、字母、下划线、中划线组成至少两位;
+	 * 域名：只能为.com ,.cn ,.net,.com.cn,.edu,.gov,.org
+	 */
+	var email_reg =/[a-zA-Z0-9_\-\.]{2,}\@\w+\.(com|cn|net|com\.cn|edu|gov|org)/;
+	return email_reg.test(email);
+}
+var test_nickName = function(nickName){
+	/**
+	 * 用户名:数字、字母、下划线(3-18位)
+	 */
+	var nickName_reg=/[a-zA-Z0-9]{3,18}/;
+	return nickName_reg.test(nickName);
+}
+
+var test_phone = function(phone){
+	/**
+	 * 电话号码:长度必须为11位,必须以1开头,第二位必须为3、4、5、6、7、8、9中的一位;
+	 */
+	var phone_reg = /^1[3456789]\d{9}/;
+	return phone_reg.test(phone);
+}
