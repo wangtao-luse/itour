8e4cb76a37e6c5efb5574abf573bb09e6c3ec0d1
wangtao-luse <wwangtaoc11@163.com>
2020-05-20 20:52:33 +0800

    init

--- itour-parent/itour-common/src/main/java/com/itour/exception/handler/DefaultExceptionHandler.java
+++ itour-parent/itour-common/src/main/java/com/itour/exception/handler/DefaultExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.itour.exception.handler;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,12 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.itour.entity.ResponseEntity;
-import com.itour.util.JsonUtil;
 /***
  * 全局异常捕获
  * @author wwang
@@ -31,36 +24,30 @@ public class DefaultExceptionHandler {
 	 * <p/>
 	 * 后续根据不同的需求定制即可
 	 */
-	@ExceptionHandler({ Throwable.class })
-	@ResponseStatus(HttpStatus.UNAUTHORIZED)
-	public ModelAndView processUnauthenticatedException(HttpServletRequest request, HttpServletResponse response,
+	@ExceptionHandler({ Throwable.class })	
+	public String processUnauthenticatedException(HttpServletRequest request, HttpServletResponse response,
 			Throwable ex) {
 		logger.error("DefaultExceptionHandler:", ex);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("exception",ex);
+		mv.setViewName("/error404");
+		
+		return "error/404";
 
-		if (!(request.getHeader("accept").indexOf("application/json") > -1
-				|| (request.getHeader("X-Requested-With") != null
-						&& request.getHeader("X-Requested-With").indexOf("XMLHttpRequest") > -1))) {
-			// 根据不同错误转向不同页面
-			ResponseEntity responseEntity = ResponseEntity.from(ex);
-			ModelAndView mv = new ModelAndView();
-			mv.addObject("error", responseEntity);
-			mv.setViewName(responseEntity.getCallbackUrl());
-			return mv;
-		} else {
-			PrintWriter out = null;
-			try {
-				ResponseEntity responseEntity = ResponseEntity.from(ex);
-				String jsonStr = JsonUtil.pojo2JsonStr(responseEntity);
-				response.setContentType("text/json;charset=UTF-8");
-				out = response.getWriter();
-				out.print(jsonStr);
-				out.flush();
-			} catch (IOException e) {
-				ModelAndView mv = new ModelAndView();
-				mv.addObject("error", e);
-				mv.setViewName("cmdty/error/exception");
-			}
-			return null;
-		}
+		/*
+		 * if (!(request.getHeader("accept").indexOf("application/json") > -1 ||
+		 * (request.getHeader("X-Requested-With") != null &&
+		 * request.getHeader("X-Requested-With").indexOf("XMLHttpRequest") > -1))) { //
+		 * 根据不同错误转向不同页面 ResponseEntity responseEntity = ResponseEntity.from(ex);
+		 * ModelAndView mv = new ModelAndView(); mv.addObject("error", responseEntity);
+		 * mv.setViewName(responseEntity.getCallbackUrl()); return mv; } else {
+		 * PrintWriter out = null; try { ResponseEntity responseEntity =
+		 * ResponseEntity.from(ex); String jsonStr =
+		 * JsonUtil.pojo2JsonStr(responseEntity);
+		 * response.setContentType("text/json;charset=UTF-8"); out =
+		 * response.getWriter(); out.print(jsonStr); out.flush(); } catch (IOException
+		 * e) { ModelAndView mv = new ModelAndView(); mv.addObject("error", e);
+		 * mv.setViewName("cmdty/error/exception"); } return null; }
+		 */
 	}
 }
