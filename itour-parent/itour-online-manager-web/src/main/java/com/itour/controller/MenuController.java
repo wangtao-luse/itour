1bed5d29ee084a1b088ae86f28c53ef3ae97698d
wwang-luse <wwangtaoc11@163.com>
2020-05-29 17:25:33 +0800

    init

--- itour-parent/itour-online-manager-web/src/main/java/com/itour/controller/MenuController.java
+++ itour-parent/itour-online-manager-web/src/main/java/com/itour/controller/MenuController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
