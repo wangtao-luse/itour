15afc7ea67a436b9c9b862a6f4f5209d19a62f92
wwang-luse <wwangtaoc11@163.com>
2020-06-12 15:44:50 +0800

    page

---
+++ itour-parent/itour-travel-service/src/main/java/com/itour/config/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.itour.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+@Configuration
+public class MybatisPlusConfig {
+	@Bean//分页
+public PaginationInterceptor  paginationInterceptor() {
+	PaginationInterceptor  paginationInterceptor  = new PaginationInterceptor();
+	paginationInterceptor.setDialectType("mysql");
+	return paginationInterceptor;
+}
+	
+}
