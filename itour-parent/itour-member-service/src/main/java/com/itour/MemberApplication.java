71f719d1cbf194351db3d9505e30b0e4b1e66bf0
wangtao-luse <wwangtaoc11@163.com>
2020-06-23 22:12:32 +0800

    init

--- itour-parent/itour-member-service/src/main/java/com/itour/MemberApplication.java
+++ itour-parent/itour-member-service/src/main/java/com/itour/MemberApplication.java
@@ -1,9 +1,13 @@
 package com.itour;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.itour.persist")
 public class MemberApplication {
 public static void main(String[] args) {
 	SpringApplication.run(MemberApplication.class, args);
--- itour-parent/itour-online-web/src/main/java/com/itour/config/ShiroConfig.java
+++ itour-parent/itour-online-web/src/main/java/com/itour/config/ShiroConfig.java
@@ -1,18 +1,13 @@
 package com.itour.config;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
