965542fdf92f28dac57d3a06ad45e2b07aa69c30
wangtao-luse <wwangtaoc11@163.com>
2020-07-01 22:58:39 +0800

    init

--- itour-parent/itour-online-manager-web/src/main/java/com/itour/controller/Student.java
+++ itour-parent/itour-online-manager-web/src/main/java/com/itour/controller/Student.java
@@ -1,7 +1,11 @@
 package com.itour.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Student {
 private String id;
@@ -41,5 +45,37 @@ public List<Student> getRecords() {
 public void setRecords(List<Student> records) {
 	this.records = records;
 }
+@Override
+public String toString() {
+	return "Student [id=" + id + ", name=" + name + ", age=" + age + ", records=" + records + "]";
+}
+public static void main(String[] args) {
+	List<Student> list = new ArrayList<Student>();
+	Student stu1 = new Student("1", "mike", "11");
+	Student stu2 = new Student("2", "mike", "16");
+	Student stu3 = new Student("3", "amy", "13");
+	Student stu4 = new Student("4", "tom", "12");
+	list.add(stu1);
+	list.add(stu2);
+	list.add(stu3);
+	list.add(stu4);
+	ArrayList<Student> collect = list.stream().collect(
+            Collectors.collectingAndThen(
+                    Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Student::getName))), ArrayList::new)
+);
+	for (Student student : collect) {
+		System.out.println(student);
+	}
+	Map<String, String> collect1 = collect.stream().collect(Collectors.toMap(p -> p.getName(),p -> p.getName()));
+	System.out.println(collect1);
+	
+	List<String> mapList = collect.stream().distinct().map(t -> t.getName()).collect(Collectors.toList());
+	for (String string : mapList) {
+		System.out.println(string);
+	}
+   
 
+
+
+}
 }
