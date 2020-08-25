db262087648755fb601b995f36b4f08bd9211be8
wwang-luse <wwangtaoc11@163.com>
2020-08-25 17:14:54 +0800

    init

--- itour-parent/itour-quartz-service/src/main/java/com/itour/config/ScheduleConfig.java
+++ itour-parent/itour-quartz-service/src/main/java/com/itour/config/ScheduleConfig.java
@@ -3,15 +3,25 @@ package com.itour.config;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import org.quartz.Trigger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 //https://www.jb51.net/article/140541.htm
+//https://blog.csdn.net/typ1805/article/details/82998351
 @Configuration
 public class ScheduleConfig implements SchedulingConfigurer {
-
+/**
+ * 在实际项目中，我们一个系统可能会定义多个定时任务。那么多个定时任务之间是可以相互独立且可以并行执行的。
+ * Spring默认会创建一个单线程池。当多个任务并发（或需要在同一时间）执行时，任务调度器就会出现时间漂移，任务执行时间将不确定。
+ * 新增一个配置类，实现SchedulingConfigurer接口。重写configureTasks方法，通过taskRegistrar设置自定义线程池。
+ */
 	@Override
 	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
 		taskRegistrar.setScheduler(taskExecutor());
@@ -21,5 +31,6 @@ public class ScheduleConfig implements SchedulingConfigurer {
 	public Executor taskExecutor() {
 		return Executors.newScheduledThreadPool(20);
 	}
+	
 }
  
---
+++ itour-parent/itour-quartz-service/src/main/java/com/itour/quartz/service/testService.java
@@ -0,0 +1,20 @@
+package com.itour.quartz.service;
+
+import javax.annotation.Resource;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Hello world!
+ *
+ */
+public class testService 
+{
+	
+
+}
