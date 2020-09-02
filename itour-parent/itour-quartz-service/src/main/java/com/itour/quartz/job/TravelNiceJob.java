228866b43fccc01b8d69e39dfdab6ba6808edd6f
wwang-luse <wwangtaoc11@163.com>
2020-09-02 11:09:02 +0800

    init

--- itour-parent/itour-quartz-service/src/main/java/com/itour/quartz/job/TravelNiceJob.java
+++ itour-parent/itour-quartz-service/src/main/java/com/itour/quartz/job/TravelNiceJob.java
@@ -43,78 +43,79 @@ private final static Logger logger=LoggerFactory.getLogger(TravelNiceJob.class);
 		
 	}
 @Transactional
-private void insertNice() {
-	try {
+	private void insertNice() {
+		try {
 
-		//1.从Redis缓存中取出点赞的数据;
-		Map<Object, Object> map = redisManager.hget(TravelRedisKey.KEY_NICE);
-		List<Nice> saveOrupdateList = new ArrayList<Nice>();
-		List<String> tidList = new ArrayList<String>();
-		List<TravelInfo> travelInfoList = new ArrayList<TravelInfo>();
-		//2.查看该用户是否已经点赞
-		  map.forEach((k,v)->{
-			  Nice nice =(Nice)v;
-			  JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(nice));
-			  RequestMessage requestMessage = HttpDataUtil.postData(jsonObject, null);
-			  ResponseMessage responseMessage = travelApi.getNice(requestMessage);
-			  if(Constant.SUCCESS_CODE.equals(responseMessage.getResultCode())) {
-				//3.同步数据到数据库	
-				  Map<String, Object> returnResult = responseMessage.getReturnResult();
-				  if(null!=returnResult.get(Constant.COMMON_KEY_RESULT)) {//点赞或取消点赞过
-					 // 修改点赞状态
-					Nice like = (Nice)returnResult.get(Constant.COMMON_KEY_RESULT);
-					nice.setId(like.getId());
-					saveOrupdateList.add(like);
-				  }else {//插入点赞表
-					  saveOrupdateList.add(nice);
-				  }
-				  
-			  }
-			  tidList.add(String.valueOf(nice.getTid()));
-		  });
-		  //批量同步数据
-		  if(saveOrupdateList.size()>0) {
-			  JSONObject tmpJSon = new JSONObject();
-			  tmpJSon.put(Constant.COMMON_KEY_ARR, saveOrupdateList);
-			  RequestMessage postData = HttpDataUtil.postData(tmpJSon, null);
-			  ResponseMessage saveOrUpdateBatchNice = travelApi.saveOrUpdateBatchNice(postData);
-		  }
-		  //4.更新点赞数
-		  JSONObject jsonObject = new JSONObject();
-		  jsonObject.put("tids", ArrayUtils.listToStr(tidList));
-		  RequestMessage postData = HttpDataUtil.postData(jsonObject, null);
-		  ResponseMessage respose = travelApi.countNiceList(postData);
-		  if(Constant.SUCCESS_CODE.equals(respose.getResultCode())) {
-			  Map<String, Object> returnResult = respose.getReturnResult();
-			  if(null!=returnResult) {
-				  List<Map<String, Object>> list =(List<Map<String, Object>>) returnResult.get(Constant.COMMON_KEY_RESULT); 
-				  for (Map<String, Object> map2 : list) {
-					  TravelInfo travelInfo = new TravelInfo();
-					  Object tid = map2.get("tid");
-					  Object count = map2.get("count");
-					  travelInfo.setId(Integer.valueOf(tid.toString()));
-					  travelInfo.setNiceCount(Integer.valueOf(count.toString()));
-					  travelInfoList.add(travelInfo);
-					
+			// 1.从Redis缓存中取出点赞的数据;
+			Map<Object, Object> map = redisManager.hget(TravelRedisKey.KEY_NICE);
+			List<Nice> saveOrupdateList = new ArrayList<Nice>();
+			List<String> tidList = new ArrayList<String>();
+			List<TravelInfo> travelInfoList = new ArrayList<TravelInfo>();
+			// 2.查看该用户是否已经点赞
+			map.forEach((k, v) -> {
+				Nice nice = (Nice) v;
+				JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(nice));
+				RequestMessage requestMessage = HttpDataUtil.postData(jsonObject, null);
+				ResponseMessage responseMessage = travelApi.getNice(requestMessage);
+				if (Constant.SUCCESS_CODE.equals(responseMessage.getResultCode())) {
+					// 3.同步数据到数据库
+					Map<String, Object> returnResult = responseMessage.getReturnResult();
+					if (null != returnResult.get(Constant.COMMON_KEY_RESULT)) {// 点赞或取消点赞过
+						// 修改点赞状态
+						Nice like = (Nice) returnResult.get(Constant.COMMON_KEY_RESULT);
+						nice.setId(like.getId());
+						saveOrupdateList.add(like);
+					} else {// 插入点赞表
+						saveOrupdateList.add(nice);
+					}
+
 				}
-			  }
-			 
-		  }
-		  jsonObject.clear();
-		  jsonObject.put(Constant.COMMON_KEY_ARR, travelInfoList);
-		  RequestMessage request = HttpDataUtil.postData(jsonObject, null);
-		  ResponseMessage updateTravelInfoBatch = this.travelApi.updateTravelInfoBatch(request);
+				tidList.add(String.valueOf(nice.getTid()));
+			});
+			// 批量同步点赞数据
+			if (saveOrupdateList.size() > 0) {
+				JSONObject tmpJSon = new JSONObject();
+				tmpJSon.put(Constant.COMMON_KEY_ARR, saveOrupdateList);
+				RequestMessage postData = HttpDataUtil.postData(tmpJSon, null);
+				ResponseMessage saveOrUpdateBatchNice = travelApi.saveOrUpdateBatchNice(postData);
+			}
+			// 4.更新点赞数
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("tids", ArrayUtils.listToStr(tidList));
+			RequestMessage postData = HttpDataUtil.postData(jsonObject, null);
+			ResponseMessage respose = travelApi.countNiceList(postData);
+			if (Constant.SUCCESS_CODE.equals(respose.getResultCode())) {
+				Map<String, Object> returnResult = respose.getReturnResult();
+				if (null != returnResult) {
+					List<Map<String, Object>> list = (List<Map<String, Object>>) returnResult
+							.get(Constant.COMMON_KEY_RESULT);
+					for (Map<String, Object> map2 : list) {
+						TravelInfo travelInfo = new TravelInfo();
+						Object tid = map2.get("tid");
+						Object count = map2.get("count");
+						travelInfo.setId(Integer.valueOf(tid.toString()));
+						travelInfo.setNiceCount(Integer.valueOf(count.toString()));
+						travelInfoList.add(travelInfo);
 
-	} catch (BaseException e) {
-		// TODO: handle exception
-		e.printStackTrace();
-		logger.error(e.getMessage());
-	}catch (Exception e) {
-		// TODO: handle exception
-		e.printStackTrace();
-		logger.error(e.getMessage());
-	}		
-}
+					}
+				}
+
+			}
+			// 批量更新点赞数
+			jsonObject.clear();
+			jsonObject.put(Constant.COMMON_KEY_ARR, travelInfoList);
+			RequestMessage request = HttpDataUtil.postData(jsonObject, null);
+			ResponseMessage updateTravelInfoBatch = this.travelApi.updateTravelInfoBatch(request);
+		} catch (BaseException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			logger.error(e.getMessage());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			logger.error(e.getMessage());
+		}
+	}
 public static void main(String[] args) {
 	//join();
 	 String [] s = { "a", "b", "c", "d", "e" };
