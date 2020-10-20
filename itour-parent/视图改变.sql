14d8d6cf2d7733edb8f7d38c297abdb51467dc7b
wwang-luse <wwangtaoc11@163.com>
2020-10-20 17:15:49 +0800

    fix

--- itour-parent/视图改变.sql
+++ itour-parent/视图改变.sql
@@ -6,10 +6,10 @@ CREATE OR  REPLACE VIEW view_a_account AS
 		utype.CNAME UTYPE_STR, 
 		accountStatus.CNAME STATUS_STR
 	FROM
-		t_a_account a,
-		( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'SEX'and d.`STATUS`='1') sex,
-		( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'UTYPE' and d.`STATUS`='1') utype,
-	  ( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'ACCOUNT_STATUS' and d.`STATUS`='1') accountStatus	
+		T_A_ACCOUNT a,
+		( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'SEX'and d.`STATUS`='1') sex,
+		( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'UTYPE' and d.`STATUS`='1') utype,
+	  ( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'ACCOUNT_STATUS' and d.`STATUS`='1') accountStatus	
 	WHERE
 		a.SEX = sex.CODE 
 	AND a.UTYPE = utype.CODE 
@@ -22,28 +22,28 @@ CREATE OR  REPLACE VIEW view_m_account AS
 		utype.CNAME UTYPE_STR, 
 		accountStatus.CNAME STATUS_STR
 	FROM
-		t_m_account a,
-		( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'SEX'and d.`STATUS`='1') sex,
-		( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'UTYPE' and d.`STATUS`='1') utype,
-	  ( SELECT d.CODE, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'ACCOUNT_STATUS' and d.`STATUS`='1') accountStatus	
+		T_M_ACCOUNT a,
+		( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'SEX'and d.`STATUS`='1') sex,
+		( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'UTYPE' and d.`STATUS`='1') utype,
+	  ( SELECT d.CODE, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'ACCOUNT_STATUS' and d.`STATUS`='1') accountStatus	
 	WHERE
 		a.SEX = sex.CODE 
 	AND a.UTYPE = utype.CODE 
 	AND a.`STATUS`=accountStatus.CODE;
 -- 字典表视图
 CREATE OR REPLACE VIEW view_d_dictionary AS
-SELECT c.*,dstatus.STATUS_STR FROM `t_d_dictionary` c ,
-(SELECT d.CODE,d.CODE_SET, d.`STATUS`,d.CNAME STATUS_STR FROM t_d_dictionary d WHERE d.CODE_SET='dictionary_status') dstatus
+SELECT c.*,dstatus.STATUS_STR FROM `T_D_DICTIONARY` c ,
+(SELECT d.CODE,d.CODE_SET, d.`STATUS`,d.CNAME STATUS_STR FROM T_D_DICTIONARY d WHERE d.CODE_SET='dictionary_status') dstatus
 WHERE c.STATUS=dstatus.`CODE`;
 --后台管理员权限表视图
 CREATE OR REPLACE VIEW view_m_right AS
-SELECT  a.*, type.CNAME MENU_TYPE_STR FROM t_m_right a,
-(SELECT c.`CODE`,c.CNAME  FROM t_d_dictionary c where c.CODE_SET='right_type') type
+SELECT  a.*, type.CNAME MENU_TYPE_STR FROM T_M_RIGHT a,
+(SELECT c.`CODE`,c.CNAME  FROM T_D_DICTIONARY c where c.CODE_SET='right_type') type
 where a.MENU_TYPE=type.`CODE`;
 ---前台会员权限表视图
 CREATE OR REPLACE VIEW view_a_right AS
-SELECT  a.*, type.CNAME MENU_TYPE_STR FROM t_a_right a,
-(SELECT c.`CODE`,c.CNAME  FROM t_d_dictionary c where c.CODE_SET='right_type') type
+SELECT  a.*, type.CNAME MENU_TYPE_STR FROM T_A_RIGHT a,
+(SELECT c.`CODE`,c.CNAME  FROM T_D_DICTIONARY c where c.CODE_SET='right_type') type
 where a.MENU_TYPE=type.`CODE`;
 ---前台会员账号查询详情
 CREATE OR REPLACE VIEW view_a_oauth AS
@@ -57,9 +57,9 @@ SELECT
 	type.CNAME OAUTH_TYPE_STR ,
 	d.CREATEDATE
 FROM
-	t_a_oauth c,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'OAUTH_TYPE' ) type,
-t_a_account	 d
+	T_A_OAUTH c,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'OAUTH_TYPE' ) type,
+T_A_ACCOUNT	 d
 WHERE
 	c.OAUTH_TYPE = type.`CODE` AND c.U_ID=d.UID;
 ---后台管理员账号查询详情
@@ -74,9 +74,9 @@ SELECT
 	type.CNAME OAUTH_TYPE_STR ,
 	d.CREATEDATE
 FROM
-	t_m_oauth c,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'OAUTH_TYPE' ) type,
-t_m_account	 d
+	T_M_OAUTH c,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'OAUTH_TYPE' ) type,
+T_M_ACCOUNT	 d
 WHERE
 	c.OAUTH_TYPE = type.`CODE` AND c.U_ID=d.UID;
 ---前台查询指定组下的会员信息
@@ -87,10 +87,10 @@ SELECT
 	s.CNAME STATUS_STR,
 	u.CNAME UTYPE_STR 
 FROM
-	`t_a_account` b
-	LEFT JOIN ( SELECT * FROM t_a_account_group ) c ON b.UID = c.U_ID,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'ACCOUNT_STATUS' AND d.`STATUS` = '1' ) s,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'UTYPE' AND d.`STATUS` = '1' ) u 
+	`T_A_ACCOUNT` b
+	LEFT JOIN ( SELECT * FROM T_A_ACCOUNT_GROUP ) c ON b.UID = c.U_ID,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'ACCOUNT_STATUS' AND d.`STATUS` = '1' ) s,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'UTYPE' AND d.`STATUS` = '1' ) u 
 WHERE
 	s.CODE = b.STATUS 
 	AND u.`CODE` = b.UTYPE;
@@ -102,19 +102,19 @@ SELECT
 	s.CNAME STATUS_STR,
 	u.CNAME UTYPE_STR 
 FROM
-	`t_m_account` b
-	LEFT JOIN ( SELECT * FROM t_m_account_group ) c ON b.UID = c.U_ID,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'ACCOUNT_STATUS' AND d.`STATUS` = '1' ) s,
-	( SELECT d.`CODE`, d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET = 'UTYPE' AND d.`STATUS` = '1' ) u 
+	`T_M_ACCOUNT` b
+	LEFT JOIN ( SELECT * FROM T_M_ACCOUNT_GROUP ) c ON b.UID = c.U_ID,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'ACCOUNT_STATUS' AND d.`STATUS` = '1' ) s,
+	( SELECT d.`CODE`, d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET = 'UTYPE' AND d.`STATUS` = '1' ) u 
 WHERE
 	s.CODE = b.STATUS 
 	AND u.`CODE` = b.UTYPE;
 ---消息列表
 CREATE OR REPLACE VIEW view_m_messageinfo AS
-	SELECT c.*,aim.CNAME AIM_STR ,origin.CNAME ORIGIN_STR FROM t_a_messageinfo c,
-	(SELECT d.`CODE`,d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET='MSG_AIM' AND d.`STATUS`='1') aim,
-	(SELECT d.`CODE`,d.CNAME FROM t_d_dictionary d WHERE d.CODE_SET='MSG_ORIGIN' AND d.`STATUS`='1') origin
+	SELECT c.*,aim.CNAME AIM_STR ,origin.CNAME ORIGIN_STR FROM T_A_MESSAGEINFO c,
+	(SELECT d.`CODE`,d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET='MSG_AIM' AND d.`STATUS`='1') aim,
+	(SELECT d.`CODE`,d.CNAME FROM T_D_DICTIONARY d WHERE d.CODE_SET='MSG_ORIGIN' AND d.`STATUS`='1') origin
 	WHERE c.AIM=aim.`CODE` and c.ORIGIN=origin.`CODE`;
 --博客列表视图
 CREATE OR REPLACE VIEW view_travelInfo_oauth AS
-SELECT c.*,a.NICKNAME FROM t_t_travel_info c, t_a_oauth a WHERE a.OAUTH_TYPE='email' AND a.U_ID=c.UID;
+SELECT c.*,a.NICKNAME FROM T_T_TRAVEL_INFO c, T_A_OAUTH a WHERE a.OAUTH_TYPE='email' AND a.U_ID=c.UID;
