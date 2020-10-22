8af0dd1b3ca6e7e4f5a8056d0fce419360f6a79c
wwang-luse <wwangtaoc11@163.com>
2020-10-22 17:30:31 +0800

    fix

--- itour-parent/itour-eureka-server/src/main/java/com/itour/EurekaApplication.java
+++ itour-parent/itour-eureka-server/src/main/java/com/itour/EurekaApplication.java
@@ -1,6 +1,7 @@
 package com.itour;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,6 +10,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 public static void main(String[] args) {
-	SpringApplication.run(EurekaApplication.class, args);
+	//https://blog.csdn.net/lisheng19870305/article/details/104822886/
+	SpringApplication springApplication = new SpringApplication(EurekaApplication.class);
+	springApplication.setWebApplicationType(WebApplicationType.SERVLET);;
+	springApplication.run(args);
+	//SpringApplication.run(EurekaApplication.class, args);
 }
 }
