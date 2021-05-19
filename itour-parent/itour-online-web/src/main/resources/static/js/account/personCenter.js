4f684d0035dc9de31a3c767840b21245304d399c
wwang-luse <wwangtaoc11@163.com>
2021-05-19 17:29:54 +0800

    文件拖拽

--- itour-parent/itour-online-web/src/main/resources/static/js/account/personCenter.js
+++ itour-parent/itour-online-web/src/main/resources/static/js/account/personCenter.js
@@ -2,6 +2,7 @@
  * 1.文件上传(拖拽|点击)--预览--裁剪--上传
  *  a.相关资料:
  *    https://www.jb51.net/article/88803.htm
+ *    https://www.huanggr.cn/1666.html
  *    文件API
  *     a.HTML5还提供了FileReader接口：用于将文件读入内存，并读取文件中的数据;
  *     b.FileReader接口提供了四个方法和六个事件
@@ -28,7 +29,7 @@ $(function(){
 			console.log(options);
 			$('.vicp-drop-area input').on("change",upload);
 			function upload(){
-				fr = new FileReader();
+			var	fr = new FileReader();
 				var file = $(".upload-dialog .vicp-drop-area input")[0].files[0];
 				fr.readAsDataURL(file);
 		    	fr.onload = function (e) {
@@ -65,7 +66,19 @@ $(function(){
 		$(".modi_dialog").find(".vicp-wrap").removeClass("maxwrap");
 		$(".upload-dialog").show();
 	});
-	var dragArea = $(".vicp-drop-area").get(0);
+	var dragArea = $(".vicp-drop-area").get(0);	
+	dragArea.ondragstart = function(e,item){
+          //判断当前浏览器是否为火狐浏览器
+		 let userAgent = navigator.userAgent;
+         let ifFirefox = userAgent.indexOf("Firefox");
+         if(ifFirefox){
+             e.dataTransfer.setData("text","Firefox");
+             e.dataTransfer.setData("imgInfo", item);
+         }
+
+		 
+		 console.log("ondragstart");
+	}
 	//1.进入目标元素触发
 	dragArea.ondragenter = function(){
 		console.log("dragenter");
@@ -81,9 +94,25 @@ $(function(){
 	}
 	//3.在目标元素上释放鼠标触发 
 	dragArea.ondrop = function(e){
-		 e.preventDefault();
-		 $(".vicp-drop-area").uploadFile({});
-		console.log("drop");	
+		e.preventDefault();
+		var fr = new FileReader();
+		var fs = e.dataTransfer.files;
+		var file = $(".upload-dialog .vicp-drop-area input")[0].files[0];
+		fr.readAsDataURL(fs[0]);
+    	fr.onload = function (e) {
+    		$(".vicp-img-container img").attr("src",e.target.result);
+    		$(".vicp-crop-right .vicp-preview-item-circle img").attr("src",e.target.result);
+    		window.close();
+    		console.log(e.target.result)
+   		};
+		console.log(fs.length);
+		$(".vicp-close").next().hide();
+		$(".vicp-step1").prev().show();
+		$(".vicp-step1").hide();
+		$(".modi_dialog").find(".vicp-wrap").addClass("maxwrap");
+		$(".vicp-step2").show();
+		console.log("drop");
+		console.log(e.dataTransfer.getData("text"));
 	}
 });
 function upload_file() {
