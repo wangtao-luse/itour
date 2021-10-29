e2745c3fc94870984bd8da12761d1bb8013197b8
wwang-luse <wwangtaoc11@163.com>
2021-10-29 17:18:58 +0800

    sub

--- itour-parent/itour-common/src/main/java/com/itour/common/tool/ItourMpGenerator.java
+++ itour-parent/itour-common/src/main/java/com/itour/common/tool/ItourMpGenerator.java
@@ -74,7 +74,7 @@ import com.baomidou.mybatisplus.generator.engine.BeetlTemplateEngine;
 			String xml_path=location+"/"+mod+"-persist"+"/src/main/resources/mapping";
 			String subStr = mod.substring(mod.indexOf("-")+1);
 			String sub=subStr.contains("-")==true?subStr.substring(0, subStr.indexOf("-")):subStr;
-			String v =isView==true?"/dto":"";
+			String v =isView==true?"/vo":"";
 			String model_path=location+"/itour-model/src/main/java/com/itour/model/"+sub+v;
 		
 			AutoGenerator mpg = new AutoGenerator();
