a7369afb2ef24e2ff274b56757a350dea4fb2c1a
wwang-luse <wwangtaoc11@163.com>
2021-12-07 11:14:14 +0800

    样式调整

--- itour-parent/itour-online-web/src/main/resources/static/js/common/rich.js
+++ itour-parent/itour-online-web/src/main/resources/static/js/common/rich.js
@@ -6,7 +6,7 @@ $(function(){
 		}
 		});	
 	//样式调整
-	$(".richContent").addClass("markdown-body editormd-preview-container").css({"width":"auto","overflow-x":"hidden"});
+	$(".richContent").addClass("markdown-body editormd-preview-container").css({"width":"auto","overflow":"hidden"});
 	$("pre").addClass("prettyprint linenums prettyprinted ");
 	$(".article_tags .tag").css({"margin":"0.25rem","color": "#0084ff"});
 	
