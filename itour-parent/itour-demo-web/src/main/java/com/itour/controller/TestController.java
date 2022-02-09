e29c8a61236d6e2d5a98e2af12c6b4f245662ca9
wwang-luse <wwangtaoc11@163.com>
2022-02-09 10:59:57 +0800

    easyui

--- itour-parent/itour-demo-web/src/main/java/com/itour/controller/TestController.java
+++ itour-parent/itour-demo-web/src/main/java/com/itour/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.itour.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itour.common.ReturnMessage;
+import com.itour.entiy.Group;
 
 import cn.hutool.json.JSONArray;
 @Controller
@@ -66,16 +70,19 @@ public class TestController{
 		o1.put("iconCls", "icon-save");
 		
 		JSONArray o1_child = new JSONArray();
-		JSONObject o1_child_o1 = new JSONObject();		
+		JSONObject o1_child_o1 = new JSONObject();	
+		o1_child_o1.put("id", "1-1");
 		o1_child_o1.put("text", "会员查询");
 		o1_child_o1.put("state", "open");
+		o1_child_o1.put("attributes",new JSONObject());
 		o1_child.add(o1_child_o1);
 		
-		JSONObject o1_child_o2 = new JSONObject();		
+		JSONObject o1_child_o2 = new JSONObject();
+		o1_child_o2.put("id", "1-2");
 		o1_child_o2.put("text", "会员组查询");
 		o1_child_o2.put("state", "open");
 		JSONObject attributes1 = new JSONObject();
-		attributes1.put("url", "/demo/book/abc");
+		attributes1.put("url", "/datagrid");
 		o1_child_o2.put("attributes", attributes1);
 		
 		o1_child.add(o1_child_o2);
@@ -84,6 +91,31 @@ public class TestController{
 		o1.put("children", o1_child);
 		jsonArray.add(o1);
 		result.setData(jsonArray);
+		return result;
+	}
+	@RequestMapping("/datagrid")
+	public String dataGridP() {
+		return "/demo-datagrid";
+	}
+	@RequestMapping("/datagrid/data")
+	@ResponseBody
+	public ReturnMessage groupList() {
+		ReturnMessage glist = this.getGlist();
+		return glist;
+		
+	}
+	public ReturnMessage getGlist() {
+		ReturnMessage result = new ReturnMessage();
+		result.setCode("10");
+		result.setMsg("操作成功！");
+		List<Group> glist = new ArrayList<Group>();
+		glist.add(new Group(1L, "王维", "2022-2-1"));
+		glist.add(new Group(1L, "苏轼", "2022-2-2"));
+		glist.add(new Group(1L, "苏辙", "2022-2-3"));
+		glist.add(new Group(1L, "李清照", "2022-2-4"));
+		Page<Group> page = new Page<Group>();
+		page.setRecords(glist);
+		result.setData(page);
 		return result;
 	}
 	
--- itour-parent/itour-demo-web/src/main/resources/static/base.js
+++ itour-parent/itour-demo-web/src/main/resources/static/base.js
@@ -1,8 +1,9 @@
-var postAjax = function(url,successFunction,options){
+//对Ajax进行封装
+var postAjax = function(url,postData,successFunction,options){
 	 var returnData;
 	var defaultOption = {
 			type : "post",
-			async : false,
+			async : true,
 			contentType : 'application/json; charset=utf-8',
 			dataType : "json",
 			errorFunction : errorFunction,
@@ -11,7 +12,8 @@ var postAjax = function(url,successFunction,options){
 	var currentOptions = $.extend(defaultOption,options);
 	$.ajax({
 		type : currentOptions.type,
-		url : url,
+		url : getContextPath() + url,
+		data:postData,
 		async : currentOptions.async,
 		contentType : currentOptions.contentType,
 		dataType : currentOptions.dataType,
@@ -50,21 +52,27 @@ var postAjax = function(url,successFunction,options){
 	});
 	return returnData;
 }
+//内部错误提示信息
 function internalError() {
     showErrorMsg(500 + " " + "程序内部错误")
 }
+//程序报错信息
 function errorFunction(resultData){
 	$.messager.alert('提示', resultData.description, 'info')
 }
+//错误信息提示
 function showErrorMsg(errorMsg) {
     $.messager.alert('提示', errorMsg, 'error')
 };
+//提示信息提示
 function showInfoMsg(errorMsg) {
     $.messager.alert('提示', errorMsg, 'info')
 };
+//调用是否成果
 function isSuccess(resultData){
 	return "10" == resultData.code;
 }
+//获取项目名
 function getContextPath() {
     // console.log(document.location.pathname);
     var pathName = document.location.pathname;
@@ -78,26 +86,45 @@ function initWestTree(url,treeNode,newOptions){
 	
 	var options ={
 			onClick: function(node){
-				 addTab({url: url, title: node.text})
+				//console.log(node.text);
+				var $this = $(this);
+				var target = node.target;
+				var id = node.id;
+				if ($this.tree('getChildren', target).length) {
+					$this.tree("toggle", target);
+	            } else{
+	            	if (node.attributes && node.attributes.url){
+	            		var url;
+                        if (node.attributes.url.indexOf('/') == 0) {
+                            url = getContextPath() + node.attributes.url;
+                        }
+	            	} else {
+	            		url = node.attributes.url;
+	            	}
+	            	addTab({url: url, title: node.text,id:id})
+	            }
+				 
 			}
 	}
 	
 	$.extend(options,newOptions);
 	sucessOption.newOptions = options;
-	postAjax(url,createWestTree,{successArguments:sucessOption});
+	postAjax(url,null,createWestTree,{successArguments:sucessOption, async: false});
 	
 }
+//初始左边的Tree成功后的回调函数
 function createWestTree(resultData,sucessOptions){
-	var options = {"data":resultData.data};
-	console.log(resultData.data);
-	$.extend(options,sucessOptions);
+	var options = {data:resultData.data};
+	console.log(options);
+	$.extend(options,sucessOptions.newOptions);
 	sucessOptions.treeNode.tree(options);
 	
 	
 }
+//添加tab页
 function addTab(params) {
     var iframe = '<iframe src="' + params.url + '" frameborder="0" style="border:0;width:100%;height:100%;"></iframe>';
-    var t = $('#index_tabs');
+    var t = $('#index-tabs');
     var opts = {
         title: params.title,
         closable: true,
@@ -113,4 +140,92 @@ function addTab(params) {
     } else {
         t.tabs('add', opts)
     }
+}
+//封装DataGrid带分页
+function initDataGrid(url,node,newOptions){
+	var options = {
+		fit:true,
+		border:false,
+		width:'auto',
+		height:'auto',
+		fitColumns:true,
+		pagination:true,
+		idField:'id',
+		sortName:'id',
+		sortOrder:'asc',
+		checkOnSelect:true,
+		singleSelect:true,
+		selectOnCheck:true,
+		nowrap:true,
+		pageNumber:1,
+		pageSize:(newOptions.postData&&newOptions.postData.page&&newOptions.postData.page.size)?newOptions.postData.page.size:10,
+		pageList: [50, 100, 300, 500, 1000],
+		toolbar:'#toolbar',
+		onLoadSuccess:function(){}
+	}
+	$.extend(options,newOptions);
+	node.datagrid(options);
+	node.datagrid('getPager').pagination({
+		onSelectPage : function(pageNumber, pageSize){
+			newOptions.postData.page.size = pageSize;
+			newOptions.postData.page.current = pageNumber;
+			initAjaxDataGrid(url,node,newOptions);
+		},
+		onChangePageSize : function(pageSize){
+			newOptions.postData.page.size = pageSize;
+			initAjaxDataGrid(url,node,newOptions);
+		},
+		onBeforeRefresh : function(pageNumber, pageSize){
+			
+		},
+		onRefresh:function(pageNumber, pageSize){
+			
+		}
+	})
+	}
+//对DataGrid进行Ajax封装
+function initAjaxDataGrid(url,node,newOptions){
+	var options = {'postData':{}};
+	$.extend(options, newOptions);
+	postAjax(url, JSON.stringify(options.postData),function(result){
+		console.log(result);
+		if(result){
+			var formatData = {};
+			formatData.total = result.data.total;
+			formatData.rows = result.data.records;
+			options.data = formatData;				
+			initDataGrid(url,node,options);
+			
+			
+		}else{
+			internalError();
+		}
+	},{});
+	
+}
+//封装dataGrid实现查询
+function searchFunc(url,node,funcName,ops){
+	  var formData = $.serializeObject($('#searchForm'));
+	  var postData = {"page":{}};
+	  ops&&ops.k?postData[ops.k] = formData:"";
+	  postData.page.size = ops&&ops.s?ops.s:"10";
+	  postData.page.current = ops&&ops.c?ops.c:"1";
+	  initAjaxDataGrid(url,node,funcName({"postData":postData}));
+	}
+//0,1,2..的对象数组转json
+//由于后台不支持传入‘'’,所以在转json的时候做过滤
+$.serializeObject = function (form) {
+  var o = {};
+  $.each(form.serializeArray(), function (index) {
+      if (o[this['name']]) {
+          o[this['name']] = filterSpecial(o[this['name']] + "," + this['value'])
+      } else {
+          o[this['name']] = filterSpecial(this['value'])
+      }
+  });
+  return o
+};
+//过滤掉"'"字符
+function filterSpecial(value) {
+    return value.replace(/[']/g, '')
 }
