47f771574fa5d2e29f7152938cfa9372d6172e07
wwang-luse <wwangtaoc11@163.com>
2022-02-11 10:59:00 +0800

    Easyui

---
+++ itour-parent/itour-demo-web/src/main/java/com/itour/dao/RightMapper.java
@@ -0,0 +1,23 @@
+package com.itour.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itour.model.Right;
+
+
+/**
+ * <p>
+ * 权限主表 Mapper 接口
+ * </p>
+ *
+ * @author wangtao
+ * @since 2020-06-23
+ */
+public interface RightMapper extends BaseMapper<Right> {
+
+}
