f4a2e12b0f59dff21e911d2ad82dffd818d8862b
wwang-luse <wwangtaoc11@163.com>
2022-06-10 15:59:44 +0800

    升级beetl模板

--- itour-parent/itour-online-web/src/main/java/com/itour/controller/IndexController.java
+++ itour-parent/itour-online-web/src/main/java/com/itour/controller/IndexController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
