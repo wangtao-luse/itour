db631d196e6d79a284d11f1953b34e5535e708a6
wwang-luse <wwangtaoc11@163.com>
2022-08-23 14:59:14 +0800

    bug修复

--- itour-parent/itour-eureka-server/src/main/java/com/itour/EurekaApplication.java
+++ itour-parent/itour-eureka-server/src/main/java/com/itour/EurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaApplication {
